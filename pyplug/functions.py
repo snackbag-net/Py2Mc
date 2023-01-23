8a9020b7d0d3b7ef80e6e8d05d643cc55fa2877d
JXSnack <jx.snack@gmail.com>
2023-01-23 18:49:16 +0100

    Recoding generator (like the entire thing) for v0.2.0
    Change notes:
    > added logging
    
    Features:
     - Can generate main file
     - Log info/warning [NEW]

--- pyplug/functions.py
+++ pyplug/functions.py
@@ -2,3 +2,7 @@ class Log:
 	@staticmethod
 	def info(text: str):
 		return "Bukkit.getLogger().info(\"" + text + "\")"
+
+	@staticmethod
+	def warn(text: str):
+		return "Bukkit.getLogger().warning(\"" + text + "\""
