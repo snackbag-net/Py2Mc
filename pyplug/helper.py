3911690d27b298a388e3b21069420fa18366e36b
JXSnack <jx.snack@gmail.com>
2023-02-10 15:58:09 +0100

    Recoding generator (like the entire thing) for v0.2.0
    Change notes:
    > Added pipInstaller and a colorlib
    
    Features:
     - Can generate main file
     - Can generate plugin.yml
     - Log info/warning
     - Send title
     - Send subtitle
     - Send actionbar
     - Send message
     - Get (almost) all player related things
     - Documented code

--- pyplug/helper.py
+++ pyplug/helper.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 
 def ifFileExistsM(path: str) -> bool:
@@ -6,3 +7,31 @@ def ifFileExistsM(path: str) -> bool:
 		os.mkdir(path)
 	except FileExistsError:
 		return True
+
+
+def pipInstaller():
+	try:
+		import requests
+	except ImportError:
+		print("[AUTOINSTALLER] A few packages aren't installed. Auto installing...")
+		os.system("python3 -m pip install --upgrade pip")
+		os.system("python3 -m pip install requests")
+		print("\n\nPlease start the script again!")
+		sys.exit(0)
+
+class Color:
+	LIGHTPURPLE = '\033[95m'
+	PURPLE = '\033[35m'
+	LIGHTCYAN = '\033[96m'
+	CYAN = '\033[36m'
+	LIGHTBLUE = '\033[94m'
+	BLUE = '\033[34m'
+	LIGHTGREEN = '\033[92m'
+	GREEN = '\033[32m'
+	LIGHTYELLOW = '\033[93m'
+	YELLOW = '\033[33m'
+	LIGHTRED = '\033[91m'
+	RED = '\033[31m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	RESET = '\033[0m'
